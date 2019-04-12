739de173f913519059ccadf89fb1734faced19f0
tranquoctrung <tranquoctrung.10k13@gmail.com>
2019-04-12 12:19:34 +0700

    remove set environment and note set invironment

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/tumlum', (req,res,next) =>
+{
+  res.send('sdhjsahdkjashdksada');
+})
+router.get('/tumlum2', (req,res,next) =>
+{
+  res.send('asdsadas');
+})
+router.get('/tumlum3', (req,res,next) =>
+{
+  res.send('asdsadas');
+})
+
+
+
+
+
+
 module.exports = router;
